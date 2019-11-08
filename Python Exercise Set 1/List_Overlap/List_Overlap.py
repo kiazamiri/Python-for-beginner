4f4e4decce27f11af5ae3163e9048bb05070a006
Mohammadkia Zamiri-Jafarian <40852909+kiazamiri@users.noreply.github.com>
2019-11-08 12:58:09 -0500

    Update List_Overlap.py

--- Python Exercise Set 1/List_Overlap/List_Overlap.py
+++ Python Exercise Set 1/List_Overlap/List_Overlap.py
@@ -1,4 +1,4 @@
-# This project generate two list of random integers with different
+# This project generates two list of random integers with different
 # sizes of 10 and 13. Then returns a list that contains the similar
 # numbers between them without duplicates.
 
